b96aba724cb3ebf3ae9fb54e336f51494d9e30f1
RohYounwoo <ywroh0619@naver.com>
2021-05-20 17:50:45 +0900

    update parkingspace example

--- kakaoAPI/index.js
+++ kakaoAPI/index.js
@@ -12,8 +12,19 @@ var geocoder = new kakao.maps.services.Geocoder();
 
 // 주소로 좌표를 검색합니다
 
-function setLocation(){
+function setLocWrapper(){
   var locVal = document.getElementById("locInput").value;
+  setLocation(locVal);
+}
+
+
+var parkLoc = ["제주특별자치도 제주시 중앙로 235", "제주특별자치도 제주시 중앙로 217", "제주특별자치도 제주시 인다14길 34", "제주특별자치도 제주시 인다14길 25", "제주특별자치도 제주시 인다14길 30", "제주특별자치도 제주시 인다14길 20"];
+var parkMarkers = [];
+for(var i = 0; i < parkLoc.length; i++){
+  setParkLoc(parkLoc[i]);
+}
+
+function setLocation(locVal){
   geocoder.addressSearch(locVal, function(result, status) {
 
       // 정상적으로 검색이 완료됐으면
@@ -43,7 +54,50 @@ function setLocation(){
           map.setCenter(coords);
       }
       else{
-        alert("검색정보 없음! 주소로 다시 입력해주세요! \nex)제주시 연동, 이도일동")
+        alert("검색정보 없음! 주소로 다시 입력해주세요! \nex)제주시 연동, 이도일동");
+      }
+  });
+}
+
+function createMarkerImage(src, size, options) {
+    var markerImage = new kakao.maps.MarkerImage(src, size, options);
+    return markerImage;
+}
+
+function setParkLoc(locVal){
+  geocoder.addressSearch(locVal, function(result, status) {
+
+      // 정상적으로 검색이 완료됐으면
+       if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+          var markerImageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/category.png';
+
+
+          var imageSize = new kakao.maps.Size(22, 26),
+            imageOptions = {
+                spriteOrigin: new kakao.maps.Point(10, 72),
+                spriteSize: new kakao.maps.Size(36, 98)
+            };
+
+          // 마커이미지와 마커를 생성합니다
+          var markerImage = createMarkerImage(markerImageSrc, imageSize, imageOptions);
+
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          var marker = new kakao.maps.Marker({
+              map: map,
+              position: coords,
+              image: markerImage
+          });
+
+          // 인포윈도우로 장소에 대한 설명을 표시합니다
+          var infowindow = new kakao.maps.InfoWindow({
+              content: '<div style="width:150px;text-align:center;padding:6px 0;">' + locVal + '</div>'
+          });
+          infowindow.open(map, marker);
+          parkMarkers.push(marker);
+          // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      }
+      else{
       }
   });
 }
